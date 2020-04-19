0b12ab0cd8c90bcdf5122f4f7b8ecc3b4ad8d918
Bryant Hayes <bryantfhayes@gmail.com>
2020-04-19 12:47:00 -0700

    update config

---
+++ tools/deploy.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+if [[ -z "${BUTLER_API_KEY}" ]]; then
+  echo "Unable to deploy! No BUTLER_API_KEY environment variable specified!"
+  exit 1
+fi
+
+prepare_butler() {
+    echo "Preparing butler..."
+    download_if_not_exist http://dl.itch.ovh/butler/linux-amd64/head/butler butler
+    chmod +x butler
+}
+
+prepare_and_push() {
+    echo "./butler push $2 $1:$3 --userversion $TRAVIS_TAG"
+    ./butler push $2 $1:$3 --userversion $TRAVIS_TAG
+}
+
+download_if_not_exist() {
+    if [ ! -f $2 ]; then
+        curl -L -O $1 > $2
+    fi
+}
+
+project="BrokenCogGames/$GAME_NAME"
+
+# make sure ENV is setup and ./butler is installed
+prepare_butler
+
+prepare_and_push $project "$GAME_NAME-windows-$TRAVIS_TAG.zip" "windows" 
+prepare_and_push $project "$GAME_NAME-linux-$TRAVIS_TAG.zip" "linux" 
+prepare_and_push $project "$GAME_NAME-html5-$TRAVIS_TAG.zip" "html5" 
+
+echo "Done."
+exit 0
