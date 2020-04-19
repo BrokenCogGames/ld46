53dc4fb41dfe9407985ab0eb2354a29dfa761c8e
Max Schweitzer <mschwei2@pdx.edu>
2020-04-19 14:18:44 -0700

    Make it better

--- scripts/Level.gd
+++ scripts/Level.gd
@@ -27,6 +27,17 @@ func win():
 func beat_game():
 	pass
 
+func _on_AphidSpawnTimer_timeout():
+	aphid_tick_count += 1
+	for aphid in aphid_spawn_table:
+		if aphid.tick_count == aphid_tick_count:
+			var scene = load("res://scenes/AphidSwarm.tscn")
+			var scene_instance = scene.instance()
+			scene_instance.set_name("AphidSwarm")
+			scene_instance.position = aphid.spawn_position
+			add_child(scene_instance)
+
+
 func gameover():
 	get_tree().paused = true
 	$Canvas/GameoverPopup.popup()
--- scripts/Level_1.gd
+++ scripts/Level_1.gd
@@ -3,13 +3,3 @@ extends "res://scripts/Level.gd"
 func _init():
 	aphid_spawn_table = [{"spawn_position": Vector2(300,300), "tick_count": 10},
 						{"spawn_position": Vector2(400,400), "tick_count": 20}]
-
-func _on_AphidSpawnTimer_timeout():
-	aphid_tick_count += 1
-	for aphid in aphid_spawn_table:
-		if aphid.tick_count == aphid_tick_count:
-			var scene = load("res://scenes/AphidSwarm.tscn")
-			var scene_instance = scene.instance()
-			scene_instance.set_name("AphidSwarm")
-			scene_instance.position = aphid.spawn_position
-			add_child(scene_instance)
